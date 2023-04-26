763fb3a4c2ceeb9ae8202a8d77180c9b95f67c11
paras231 <sharmaparas553@gmail.com>
2023-04-26 22:46:15 +0530

    updatde

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,75 @@
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
+import express from "express";
+import http from "http";
+import cors from "cors";
 import { Server } from "socket.io";
 
 const PORT = 5000;
-const app  = express();
+const app = express();
 
 const server = http.createServer(app);
 
-const io  = new Server(server,{
-    cors:{
-        origin:"http://localhost:3000"
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 const testData = [
-    {
-        name:"Paras",
-        email:"sharma@gmail.com"
-    },
-    {
-        name:"Lokesh",
-        email:"lokesh@gmail.com"
-    },
-    {
-        name:'Jayesh',
-        email:'jayesh@gail.com'
-    },
-    {
-        name:'Ajay'
-    }
-]
-io.on("connection",(socket)=>{
-    console.log('user connected')
-    console.log(socket.id);
-    socket.emit('data',testData)
+  {
+    name: "Paras",
+    email: "sharma@gmail.com",
+  },
+  {
+    name: "Lokesh",
+    email: "lokesh@gmail.com",
+  },
+  {
+    name: "Jayesh",
+    email: "jayesh@gail.com",
+  },
+  {
+    name: "Ajay",
+  },
+];
 
-    socket.on('disconnect',()=>{
-        console.log('user left');
-    })
-})
+function sendData() {
+  return {
+    user: {
+      userName: "Paras Sharma",
+    },
+  };
+}
+
+let response = {}
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log(socket.id);
+  socket.emit("data", testData);
+
+  const typing = "user is not  typing...";
+
+  socket.emit("typing", typing);
+
+  socket.on("userdata", (data) => {
+    // this data will come from frontend
+    response = data;
+    console.log(response);
+     // send this received response to client again->
+    socket.emit('response',response);
+  });
+
+ 
+
+  
+
+  socket.on("disconnect", () => {
+    console.log("user left");
+  });
+});
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-
-
-server.listen(PORT,()=>{
-    console.log(`server running on ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,85 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
 let socket;
 const App = () => {
+  const [data, setData] = useState([]);
+  const [typing, setTyping] = useState("");
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+  });
+  const [response, setResponse] = useState({});
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
 
-  const [data,setData] = useState([]);
-
-  useEffect(()=>{
+  useEffect(() => {
     socket = io("http://localhost:5000");
-    console.log(socket);
-    socket.on('connection',()=>{
-console.log('socket connected',socket);
-    })
-  },[])
+    // console.log(socket);
+    socket.on("connection", () => {
+      console.log("socket connected", socket);
+    });
+  }, []);
+  // console.log(socket);
+  useEffect(() => {
+    socket.on("data", (newdata) => {
+      // console.log(newdata);
+      setData([...data, newdata]);
+    });
+  }, [data]);
 
-  useEffect(()=>{
-    socket.on("data",(newdata)=>{
-      console.log(newdata);
-      setData([...data,newdata])
-    })
-  },[data])
-console.log(data[0],'line 24');
-const updated = data[0];
+  console.log(typing);
+
+  useEffect(() => {
+    socket.on("typing", (data) => {
+      // console.log(data);
+      setTyping(data);
+    });
+  }, [typing]);
+
+  useEffect(() => {
+    // sending input data from frontend to backend in socket
+    socket.emit("userdata", userData);
+  }, [userData]);
+
+  useEffect(() => {
+    socket.on("response", (res) => {
+      setResponse(res);
+    });
+  },[response]);
+  console.log(response);
   return (
     <>
-    {/* <h3>socket io</h3> */}
-    {
-     updated?.map((value)=>{
-      return (
-        <>
-        <h3>{value.name}</h3>
-        </>
-      )
-     })
-    }
+      {/* <h3>socket io</h3> */}
+      {/* {updated?.map((value) => {
+        return (
+          <>
+            <h3>{value.name}</h3>
+          </>
+        );
+      })} */}
+      {/* <input type="text" placeholder={typing ? typing : "Write something"} /> */}
+      <input
+        type="text"
+        name="name"
+        value={userData.name}
+        placeholder="Enter name"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="email"
+        value={userData.email}
+        placeholder="Enter email"
+        onChange={handleChange}
+      />
+      <h2>{response?.name}</h2>
+      <h2>{response?.email}</h2> 
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

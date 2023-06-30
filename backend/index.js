02bc8f8e13cc933bd4fccbdb169f00fe0c1b145a
paras231 <sharmaparas553@gmail.com>
2023-06-30 16:53:51 +0530

    updated code

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,22 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import { Server } from "socket.io";
-
+import chatRoute from "./routes/chat.js";
 const PORT = 5000;
 const app = express();
 
 const server = http.createServer(app);
 
+app.use(express.json({ limit: "500mb" }));
+app.use(cors());
+app.use("/api/chat", chatRoute);
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
   },
 });
+
 const testData = [
   {
     name: "Paras",
@@ -39,7 +44,7 @@ function sendData() {
   };
 }
 
-let response = {}
+let response = {};
 
 io.on("connection", (socket) => {
   console.log("user connected");
@@ -54,21 +59,18 @@ io.on("connection", (socket) => {
     // this data will come from frontend
     response = data;
     console.log(response);
-     // send this received response to client again->
-    socket.emit('response',response);
+    // send this received response to client again->
+    socket.emit("response", response);
   });
 
- 
-
-  
+  socket.on("newdata", (res) => {
+    console.log(res);
+  });
 
   socket.on("disconnect", () => {
     console.log("user left");
   });
 });
-
-app.use(express.json());
-app.use(cors());
 
 server.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
